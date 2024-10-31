ad35ae328ecb97797d73726fca2dabbf33b08ba7
usamaElgendy <48976562+usamaaelgendy@users.noreply.github.com>
2024-10-31 15:18:35 +0300

    Coin Dto

---
+++ app/src/main/java/com/elgendy/cryptocurrencyapp/data/remote/CoinPaprikaApi.kt
@@ -0,0 +1,13 @@
+package com.elgendy.cryptocurrencyapp.data.remote
+
+import com.elgendy.cryptocurrencyapp.data.remote.dto.CoinDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface CoinPaprikaApi {
+    @GET("/v1/coins")
+    suspend fun getCoins(): List<CoinDto>
+
+    @GET("/v1/coins/{coinId}")
+    suspend fun getCoinById(@Path ("coinId") coinId: String): CoinDto
+}
